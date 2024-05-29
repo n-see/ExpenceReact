ab776225648842cbb983e70a428262b03df24feb
Neo See <neococo123@yahoo.com>
2024-05-28 19:49:24 -0700

    created delete function

--- expenseReact/src/App.tsx
+++ expenseReact/src/App.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react"
+import ExpenseList from "./expense-tracker/components/ExpenseList"
 
 
 const App = () => {
+  
+  const [dummyExpensesArray, setDummyExpensesArray] = useState([
+    {id: 1, description: 'aaa', amount:10, category: 'Utilities'},
+    {id: 2, description: 'bbb', amount:15, category: 'Entertainment'},
+    {id: 3, description: 'ccc', amount:20, category: 'Food'},
+    {id: 4, description: 'ddd', amount:25, category: 'Shop[ping'},
+    {id: 5, description: 'eee', amount:16, category: 'Groceries'},
+  ])
+  
+  const handleDelete = (id:number) => {
+    setDummyExpensesArray(dummyExpensesArray.filter(expense => expense.id != id))
+  }
   return (
     <>
       <h1 className="text-center">Expense Tracker</h1>
+      <ExpenseList expenses={dummyExpensesArray} onDelete={handleDelete}/>
     </>
   )
 }
