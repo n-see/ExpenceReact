6379523a4a09341867d953f77c7ff4d33f1e4ad4
Neo See <neococo123@yahoo.com>
2024-09-23 21:24:34 -0700

    push

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf553dac86b9131902396d28cb9116301eaa447c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+399c405e207f95bfde363cf9b8ab8c103e28db8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
