b8c009b611702669be6e01dc671240d4cf6d32c8
Neo See <neococo123@yahoo.com>
2024-09-24 20:45:28 -0700

    please work

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3775b7837a3d87d32dadd48129e2eb3a865d6821")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4868b12878e38ee0fcf6853a1a17d9bd9dc5a72b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
