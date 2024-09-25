06ef7cde9aeaf3b9c6911603bf10594bef1d1415
Neo See <neococo123@yahoo.com>
2024-09-24 20:30:11 -0700

    partial working

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6379523a4a09341867d953f77c7ff4d33f1e4ad4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6d8874b89f57b5fd2b9cb1b1e83050c0558e76d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
